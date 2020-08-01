85079305f80e6330304f540cb54a622194d4e013
Andre Pinheiro <andreftmarcelo@gmail.com>
2020-08-01 22:29:45 +0100

    Add animations on message box

--- src/messages.js
+++ src/messages.js
@@ -1,7 +1,9 @@
 import {
     blackWindow,
     messageContent,
+    informationButtom,
     rules,
+    contact,
     container,
     singleTitle,
     toy
@@ -12,11 +14,29 @@ rules.onclick = () => {
     const text = 'As regras são simples! Ao começar o jogo aparecerá três imagens de forma aléatoria e três botões. Cada um desses botões irá corresponder a uma imagem, mas CUIDADO! Exite a chance de aparecer armadilhas... De qualquer forma, boa sorte em obter a maior pontuação maxima :) '
     messageContent.appendChild(singleTitle)
     messageContent.appendChild(document.createTextNode(text))
+    messageContent.style.height = '180px'
     
     
     container.appendChild(blackWindow)
     container.appendChild(toy)
     container.appendChild(messageContent)
+}
+
+contact.onclick = () => {
+    singleTitle.appendChild(document.createTextNode('Contacto!'))
+    const text = 'Os contanctos não estão disponíveis! Aguarde até o C4INAN resolver isso!'
+    messageContent.appendChild(singleTitle)
+    messageContent.appendChild(document.createTextNode(text))
+    messageContent.style.height = '80px'
+    
+    
+    container.appendChild(blackWindow)
+    container.appendChild(toy)
+    container.appendChild(messageContent)
+}
+
+informationButtom.onclick = () => {
+    
 }
 
 
@@ -27,3 +47,4 @@ blackWindow.onclick = () => {
     container.removeChild(blackWindow)
     container.removeChild(messageContent)
 } 
+
